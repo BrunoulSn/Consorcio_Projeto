33b7705971c0b56f3424d5cbd6003d88ed678d3f
Bruno Cristiano <bruno120505@gmail.com>
2025-05-29 21:08:42 -0300

    Atualizado

--- consorcio/indexController.js
+++ consorcio/indexController.js
@@ -1,10 +1,10 @@
-let apiUrl = 'http://localhost:8080/consorcios';
+let apiUrl = 'http://localhost:8081/consorciosalan';
 
 fetch(apiUrl)
   .then(response => response.json())
   .then(data => {
     console.log('Consórcios   :', data);
-    displayConsorcios(data); // 👈 CHAMADA PARA EXIBIR NA TELA
+    displayConsorcios(data); //  CHAMADA PARA EXIBIR NA TELA
   })
   .catch(error => {
     console.error('Erro ao buscar consórcios:', error);
@@ -16,7 +16,25 @@ function displayConsorcios(consorcios) {
 
   consorcios.forEach(item => {
     const div = document.createElement('div');
-    div.textContent = `${item.nome} - Valor: R$${item.valor.toFixed(2)}`;
-    gallery.appendChild(div);
+const titulo = document.createElement('h1');
+const valor = document.createElement('p');
+
+// Adicionando as classes
+div.classList.add('consorcioLista');
+titulo.classList.add('tituloConsorcioLista');
+valor.classList.add('valorConsorcioLista');
+
+// Definindo o conteúdo dos elementos
+titulo.textContent = `${item.nome}`;
+valor.textContent = `Valor: R$${item.valor.toFixed(2)}`;
+
+// Adicionando os elementos ao 'div'
+div.appendChild(titulo);
+div.appendChild(valor);
+
+// Adicionando a 'div' ao gallery
+gallery.appendChild(div);
+
   });
 }
+fetch(apiUrl)
